7ae33a908043ce158c44c6d5afee38ae4696a69a
erem2k <nope@nohost.com>
2025-04-05 17:24:31 +0200

    bin - move binpatching libs from sources  to bin directory

--- src/networking/networking.h
+++ src/networking/networking.h
@@ -11,31 +11,6 @@
 #ifndef PROTOLIBS_DEFD_H
 #define PROTOLIBS_DEFD_H
 
-
-// hack because theres no macro for DEBUG that i can find
-// feel free to PR to add it to vpc if you know of one
-#ifdef DEBUG
-    // x86-windows-static-md
-    /*
-    set(VCPKG_TARGET_ARCHITECTURE x86)
-    set(VCPKG_CRT_LINKAGE static)
-    set(VCPKG_LIBRARY_LINKAGE static)
-    set(protobuf_MSVC_STATIC_RUNTIME ON)
-
-    set(_ITERATOR_DEBUG_LEVEL 0)
-    set(VCPKG_C_FLAGS "-D_ITERATOR_DEBUG_LEVEL=0")
-    set(VCPKG_CXX_FLAGS "-D_ITERATOR_DEBUG_LEVEL=0")
-    */
-    #pragma comment( lib, "../shared/sdk13-gigalib/bin/libprotobufd.lib" )
-#else
-    #pragma comment( lib, "../shared/sdk13-gigalib/bin/libprotobuf.lib" )
-#endif
-
-#endif
-#endif
-
-
-
 // #define OVERRIDE override
 
 #include "steam/isteamclient.h"
